dd99b295a07df0999b31ab714025ad10b63a896c
Prinston Dsouza <prinstondsouza42@gmail.com>
2026-01-14 10:40:37 +0530

    access token expiration set to 10 seconds for testing

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -75,7 +75,7 @@ export const loginUser = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, userType: "user" },
       process.env.JWT_SECRET,
-      { expiresIn: "7d" }
+      { expiresIn: "10s" }
     );
 
     res.json({
